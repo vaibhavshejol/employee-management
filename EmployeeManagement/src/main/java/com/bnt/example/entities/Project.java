c1b611f840d2d06febea039b5931bba6fc2b8ee8
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-30 17:14:46 +0530

    Employee list added as field in project entity

--- EmployeeManagement/src/main/java/com/bnt/example/entities/Project.java
+++ EmployeeManagement/src/main/java/com/bnt/example/entities/Project.java
@@ -1,10 +1,13 @@
 package com.bnt.example.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,4 +25,6 @@ public class Project {
     String name;
     @Column(name = "project_location")
     String location;
+    @ManyToMany
+    List<Employee> employees;
 }
