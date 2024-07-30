1fb3de7db078678b793f8abbcc58acf700d96058
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-30 17:25:34 +0530

    Service layer created for project entity

---
+++ EmployeeManagement/src/main/java/com/bnt/example/service/ProjectService.java
@@ -0,0 +1,5 @@
+package com.bnt.example.service;
+
+public interface ProjectService {
+    
+}
---
+++ EmployeeManagement/src/main/java/com/bnt/example/service/serviceimpl/ProjectServiceImpl.java
@@ -0,0 +1,10 @@
+package com.bnt.example.service.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.bnt.example.service.ProjectService;
+
+@Service
+public class ProjectServiceImpl implements ProjectService {
+    
+}
