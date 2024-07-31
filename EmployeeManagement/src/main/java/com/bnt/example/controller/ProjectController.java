4bbf6ead33eb8e451a36789e17da3e1efb22e32e
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 13:03:15 +0530

    API created for create project

--- EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
+++ EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
@@ -1,8 +1,15 @@
 package com.bnt.example.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bnt.example.entities.Project;
+import com.bnt.example.response.SuccessResponse;
 import com.bnt.example.service.ProjectService;
 
 @RestController
@@ -15,6 +22,11 @@ public class ProjectController {
         this.projectService=projectService;
     }
 
-    
+    @PostMapping
+    public ResponseEntity<SuccessResponse> createProject(@RequestBody Project project){
+        Project createdProject = projectService.addProject(project);
+        SuccessResponse response = new SuccessResponse("Project added successfully.....", createdProject, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
 }
