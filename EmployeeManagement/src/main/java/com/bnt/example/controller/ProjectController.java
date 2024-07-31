3b15e7929c168cbc753a8639cf820d3ed4f22ba7
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 12:56:49 +0530

    Dependency injected using contructor in project controller

--- EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
+++ EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
@@ -3,8 +3,18 @@ package com.bnt.example.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bnt.example.service.ProjectService;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
+
+    private ProjectService projectService;
+
+    public ProjectController(ProjectService projectService){
+        this.projectService=projectService;
+    }
+
     
+
 }
