fdd7c1711f64d00423b065e42dd972300299dc1d
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 16:49:24 +0530

    Assigned column name to the name field of company entity

--- EmployeeManagement/src/main/java/com/bnt/example/entities/Company.java
+++ EmployeeManagement/src/main/java/com/bnt/example/entities/Company.java
@@ -1,10 +1,12 @@
 package com.bnt.example.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 @Embeddable
 @Data
 public class Company {
+    @Column(name = "company_name")
     String name;
 }
