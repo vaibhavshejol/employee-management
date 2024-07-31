17ca2ad5d0e968459ac9a879d95c5291bfa1cb71
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 14:53:45 +0530

    Wrote service for add project

--- EmployeeManagement/src/main/java/com/bnt/example/service/ProjectService.java
+++ EmployeeManagement/src/main/java/com/bnt/example/service/ProjectService.java
@@ -1,5 +1,9 @@
 package com.bnt.example.service;
 
+import com.bnt.example.entities.Project;
+
 public interface ProjectService {
+
+    Project addProject(Project project);
     
 }
--- EmployeeManagement/src/main/java/com/bnt/example/service/serviceimpl/ProjectServiceImpl.java
+++ EmployeeManagement/src/main/java/com/bnt/example/service/serviceimpl/ProjectServiceImpl.java
@@ -2,6 +2,8 @@ package com.bnt.example.service.serviceimpl;
 
 import org.springframework.stereotype.Service;
 
+import com.bnt.example.entities.Project;
+import com.bnt.example.exception.UnknownException;
 import com.bnt.example.repository.ProjectRepository;
 import com.bnt.example.service.ProjectService;
 
@@ -12,5 +14,17 @@ public class ProjectServiceImpl implements ProjectService {
 
     public ProjectServiceImpl(ProjectRepository projectRepository){
         this.projectRepository=projectRepository;
+    }
+
+    @Override
+    public Project addProject(Project project) {
+        try{
+            if(project==null || project.getName()==null || project.getLocation()==null){
+                throw new UnknownException("Project object or field cannot be null.");
+            }
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new UnknownException(e.getMessage());
+        }
     }
 }
