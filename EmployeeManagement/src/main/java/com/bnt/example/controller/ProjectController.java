143cb29e86ee296ab4dbfe5a008f4cd6281cc0bc
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 14:24:41 +0530

    API created for get all projects

--- EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
+++ EmployeeManagement/src/main/java/com/bnt/example/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.bnt.example.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,13 @@ public class ProjectController {
         Project createdProject = projectService.addProject(project);
         SuccessResponse response = new SuccessResponse("Project added successfully.....", createdProject, HttpStatus.CREATED);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse> getProjects(){
+        List<Project> existedProjects = projectService.getProjects();
+        SuccessResponse response = new SuccessResponse("Project list:", existedProjects, HttpStatus.FOUND);
+        return ResponseEntity.status(HttpStatus.FOUND).body(response);
     }
 
 }
