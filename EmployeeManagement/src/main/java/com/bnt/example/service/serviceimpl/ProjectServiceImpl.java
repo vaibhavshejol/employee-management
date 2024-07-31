19df718435edecd692334f344a8f422f7802572e
Vaibhav Shejol <vaibhav.shejol@bnt.local>
2024-07-31 14:48:11 +0530

    Dependency injected using contructor

--- EmployeeManagement/src/main/java/com/bnt/example/service/serviceimpl/ProjectServiceImpl.java
+++ EmployeeManagement/src/main/java/com/bnt/example/service/serviceimpl/ProjectServiceImpl.java
@@ -2,9 +2,15 @@ package com.bnt.example.service.serviceimpl;
 
 import org.springframework.stereotype.Service;
 
+import com.bnt.example.repository.ProjectRepository;
 import com.bnt.example.service.ProjectService;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
     
+    private ProjectRepository projectRepository;
+
+    public ProjectServiceImpl(ProjectRepository projectRepository){
+        this.projectRepository=projectRepository;
+    }
 }
